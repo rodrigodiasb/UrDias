520c10fe2d16ce51c16954f83671f975a06c41b9
rodrigodiasb <62857786+rodrigodiasb@users.noreply.github.com>
2026-01-07 08:36:20 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { AppStateProvider } from "./state.js";
+import DaysScreen from "./screens/DaysScreen.jsx";
+import DayScreen from "./screens/DayScreen.jsx";
+import EvaluationScreen from "./screens/EvaluationScreen.jsx";
+
+export default function App() {
+  return (
+    <AppStateProvider>
+      <Routes>
+        <Route path="/" element={<DaysScreen />} />
+        <Route path="/day/:dayId" element={<DayScreen />} />
+        <Route path="/day/:dayId/eval/new" element={<EvaluationScreen mode="new" />} />
+        <Route path="/day/:dayId/eval/:evId" element={<EvaluationScreen mode="edit" />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </AppStateProvider>
+  );
+}
